37558e4bf3a120aa04a9f128f0b049e73845fd82
sultonov-f <sultonovfayyozbek@gmail.com>
2025-12-10 17:00:58 +0500

    System check!

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 using namespace std;
 int main() {
-  cout << "Hello Nio!";
+  system("cls");
+  cout << "Hello Nio!\n";
+  cout << "\033[13mRed text\033[0m\n";
+  system("pause");
   return 0;
 }
